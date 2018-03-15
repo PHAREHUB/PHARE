d26f9ce9471ec7ac8e6eab35f22659ad2f373237
Thibault Payet <monwarez@gmail.com>
2018-03-15 15:58:54 +0100

    Add clang format, and add field for test purpose

---
+++ src/core/data/field/field.h
@@ -0,0 +1,17 @@
+#ifndef PHARE_CORE_DATA_FIELD_FIELD_H
+#define PHARE_CORE_DATA_FIELD_FIELD_H
+
+#include <cstdint>
+
+namespace PHARE
+{
+class Field
+{
+private:
+    uint32_t size_{0};
+
+public:
+    uint32_t size() const { return size_; }
+};
+}
+#endif
