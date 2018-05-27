1904b463e0938a84ff6c22b299c79fad43e4c828
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2018-05-27 10:28:38 +0200

    add implementation for ampere1D operator()

--- src/core/numerics/ampere/ampere.h
+++ src/core/numerics/ampere/ampere.h
@@ -4,6 +4,10 @@
 #include <cstddef>
 #include <iostream>
 
+#include "data/grid/gridlayoutdefs.h"
+#include "data/vecfield/vecfield_component.h"
+#include "utilities/index/index.h"
+
 namespace PHARE
 {
 template<typename GridLayout, std::size_t dim>
@@ -45,8 +49,33 @@ public:
     template<typename VecField>
     void operator()(VecField const &B, VecField &J)
     {
-        this->layout_->deriv();
-        std::cout << "solving ampere\n";
+        // Jx = 0
+        // Jy = dzBx - dxBz = -dxBz
+        // Jz = dxBy - dyBx = dxBy
+        auto &Jx = J.getComponent(Component::X);
+        auto &Jy = J.getComponent(Component::Y);
+        auto &Jz = J.getComponent(Component::Z);
+
+        auto const &Bx = J.getComponent(Component::X);
+        auto const &By = J.getComponent(Component::Y);
+        auto const &Bz = J.getComponent(Component::Z);
+
+        // Direction should not be in gridlayoutdef but i utilities somehow
+        auto start = this->layout_->physicalStartIndex(Jy, Direction::X);
+        auto end   = this->layout_->physicalEndIndex(Jy, Direction::X);
+
+        for (auto ix = start; ix <= end; ++ix)
+        {
+            Jy(ix) = -this->layout_->deriv(Bz, make_index(ix), DirectionTag<Direction::X>{});
+        }
+
+        start = this->layout_->physicalStartIndex(Jz, Direction::X);
+        end   = this->layout_->physicalEndIndex(Jz, Direction::X);
+
+        for (auto ix = start; ix <= end; ++ix)
+        {
+            Jz(ix) = this->layout_->deriv(By, make_index(ix), DirectionTag<Direction::X>{});
+        }
     }
 };
 
