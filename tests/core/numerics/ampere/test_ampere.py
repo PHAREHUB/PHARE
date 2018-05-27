e346be07fd654f6f9c24ffe962309e74768ae466
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2018-05-27 13:30:51 +0200

    add python scripts for expected values of ampere test 1D

--- tests/core/numerics/ampere/test_ampere.py
+++ tests/core/numerics/ampere/test_ampere.py
@@ -6,12 +6,57 @@
 
 
 import numpy as np
+import sys
+import os
+
+def test_ampere_yee1D(path):
+
+    # since the derivative is tested for all interporders
+    # we don't do ampere for all interporders, only for interporder=1
+
+    nbrCellsX = 50
+    dualAllocSize = 52
+    primalAllocSize = 53
+    meshSize=0.1
+    xmax = 5.
+
+    Jx = np.zeros(dualAllocSize, dtype=np.float64)
+    Jy = np.zeros(primalAllocSize, dtype=np.float64)
+    Jz = np.zeros(primalAllocSize, dtype=np.float64)
+
+    psi_p = 1
+    pei_p = 51
+    psi_d = 1
+    pei_d = 50
+
+    x_dual = meshSize*np.arange(dualAllocSize) - meshSize/2.
+    x_primal = meshSize*np.arange(primalAllocSize) - meshSize
+
+    By = np.cos(2*np.pi/xmax * x_dual)
+    Bz = np.sin(2*np.pi/xmax * x_dual)
+
+    # Jy = dzBx - dxBz  -dxBz
+    # Jz = dxBy - dyBx   dxBy
+    Jy[psi_p:pei_p+1] = -( Bz[psi_d:pei_d+2]  - Bz[psi_d-1:pei_d+1])/meshSize
+    Jz[psi_p:pei_p+1] =  ( By[psi_d:pei_d+2]  - By[psi_d-1:pei_d+1])/meshSize
 
 
-def main():
+    filename_jy = "jy_yee_1D_order1.txt"
+    filename_jz = "jz_yee_1D_order1.txt"
+
+    np.savetxt(os.path.join(path, filename_jy), Jy, delimiter=" ")
+    np.savetxt(os.path.join(path, filename_jz), Jz, delimiter=" ")
+
+
+def main(path='./'):
+
+    if len(sys.argv) >1:
+        path=sys.argv[1]
+
+    test_ampere_yee1D(path)
 
 
 
 
-if __name__ == '__main_'':
+if __name__ == '__main__':
     main()
