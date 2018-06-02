18de02f71021cee7e8b1d74a9f8403664b5f3f3f
nicolasaunai <nicolas.aunai@lpp.polytechnique.fr>
2018-06-02 21:02:30 +0200

    simplify reset resource in guard using std apply

--- src/amr/tools/resources_guards.h
+++ src/amr/tools/resources_guards.h
@@ -23,28 +23,6 @@ namespace PHARE
 template<typename ResourcesManager, typename... ResourcesUsers>
 class ResourcesGuard
 {
-private:
-    template<typename RequestedPtr, typename ResourcesUser>
-    void setResources_(RequestedPtr requestedPtr, ResourcesUser& resourcesUser) const
-    {
-        resourcesManager_.setResources_(resourcesUser, requestedPtr, patch_);
-    }
-    template<typename RequestedPtr, typename ResourcesUser, typename... ResourcesUserTail>
-    void setResources_(RequestedPtr requestedPtr, ResourcesUser& resourcesUser,
-                       ResourcesUserTail&... resourcesUsers) const
-    {
-        setResources_(requestedPtr, resourcesUser);
-        setResources_(requestedPtr, resourcesUsers...);
-    }
-
-
-    template<std::size_t... I>
-    void resetResources_(std::index_sequence<I...>) const
-    {
-        setResources_(UseNullPtr{}, std::get<I>(resourcesUsers_)...);
-    }
-
-
 public:
     /**
      *  \brief At construction, each resourcesUser will be valid. At destruction time, each
@@ -62,30 +40,51 @@ public:
     {
         setResources_(UseResourcePtr{}, resourcesUsers...);
     }
+
+
+
+
     ~ResourcesGuard()
     {
-        // Here we want to do a loop on the tuple : resourcesUsers_
-        // we will do it in a recursive way, first with indexes we get a vector like that
-        // 0 , 1 , 2 , ...
-        // then we call setResources with the variadic arguments obtained by unpacking the tuple
-        // with std::get
-        auto indexes
-            = std::make_index_sequence<std::tuple_size<decltype(resourcesUsers_)>::value>{};
-        resetResources_(indexes);
+        // set nullptr to all users in resourcesUsers_
+        std::apply([this](auto&... user) { ((setResources_(UseNullPtr{}, user)), ...); },
+                   resourcesUsers_);
     }
 
-    // We just need the move constructor for using ResourceManager::createResourcesGuards
-    ResourcesGuard(ResourcesGuard&&) = default;
 
+
+
+    // We just need the move constructor for using ResourceManager::createResourcesGuards
+    ResourcesGuard(ResourcesGuard&&)      = default;
     ResourcesGuard()                      = delete;
     ResourcesGuard(ResourcesGuard const&) = delete;
     ResourcesGuard& operator=(ResourcesGuard const& source) = delete;
     ResourcesGuard& operator=(ResourcesGuard&&) = delete;
 
+
+
 private:
     std::tuple<ResourcesUsers&...> resourcesUsers_;
     SAMRAI::hier::Patch const& patch_;
     ResourcesManager const& resourcesManager_;
+
+private:
+    template<typename RequestedPtr, typename ResourcesUser>
+    void setResources_(RequestedPtr requestedPtr, ResourcesUser& resourcesUser) const
+    {
+        resourcesManager_.setResources_(resourcesUser, requestedPtr, patch_);
+    }
+
+
+
+
+    template<typename RequestedPtr, typename ResourcesUser, typename... ResourcesUserTail>
+    void setResources_(RequestedPtr requestedPtr, ResourcesUser& resourcesUser,
+                       ResourcesUserTail&... resourcesUsers) const
+    {
+        setResources_(requestedPtr, resourcesUser);
+        setResources_(requestedPtr, resourcesUsers...);
+    }
 };
 } // namespace PHARE
 #endif
