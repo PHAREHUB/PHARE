865f6f261b376ffc5eab06248818c9d9e0690052
Thibault Payet <monwarez@gmail.com>
2018-08-22 14:28:16 +0200

    add dim 2 and 3 for time interpolation

--- src/amr/data/field/time_interpolate/field_linear_time_interpolate.h
+++ src/amr/data/field/time_interpolate/field_linear_time_interpolate.h
@@ -88,6 +88,50 @@ public:
                 fieldDest(ix) = (1. - alpha) * fieldSrcOld(ix) + alpha * fieldSrcNew(ix);
             }
         }
+        else if constexpr (dim == 2)
+        {
+            auto iStartX = localBox.lower(dirX);
+            auto iEndX   = localBox.upper(dirX);
+
+            auto iStartY = localBox.lower(dirY);
+            auto iEndY   = localBox.upper(dirY);
+
+            for (auto ix = iStartX; ix <= iEndX; ++ix)
+            {
+                for (auto iy = iStartY; iy <= iEndY; ++iy)
+                {
+                    fieldDest(ix, iy)
+                        = (1. - alpha) * fieldSrcOld(ix, iy) + alpha * fieldSrcNew(ix, iy);
+                }
+            }
+        }
+        else if constexpr (dim == 3)
+        {
+            auto iStartX = localBox.lower(dirX);
+            auto iEndX   = localBox.upper(dirX);
+
+            auto iStartY = localBox.lower(dirY);
+            auto iEndY   = localBox.upper(dirY);
+
+            auto iStartZ = localBox.lower(dirZ);
+            auto iEndZ   = localBox.upper(dirZ);
+
+            for (auto ix = iStartX; ix <= iEndX; ++ix)
+            {
+                for (auto iy = iStartY; iy <= iEndY; ++iy)
+                {
+                    for (auto iz = iStartZ; iz <= iEndZ; ++iz)
+                    {
+                        fieldDest(ix, iy, iz) = (1. - alpha) * fieldSrcOld(ix, iy, iz)
+                                                + alpha * fieldSrcNew(ix, iy, iz);
+                    }
+                }
+            }
+        }
+        else
+        {
+            static_assert(dim >= 0 && dim <= 3);
+        }
     }
 
 
