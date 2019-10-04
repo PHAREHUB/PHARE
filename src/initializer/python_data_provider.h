a27f2ad31688c743c87baca25dc2d4da4a50ee6f
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2019-10-04 11:34:39 +0200

    can load pyphare from test

--- src/initializer/python_data_provider.h
+++ src/initializer/python_data_provider.h
@@ -5,10 +5,12 @@
 
 #include "pragma_disable.h"
 
+// clang-format off
 DISABLE_WARNING(shadow, shadow-field-in-constructor-modified, 42)
 #include <pybind11/embed.h> // everything needed for embedding
 #include <pybind11/functional.h>
 ENABLE_WARNING(shadow, shadow-field-in-constructor-modified, 42)
+// clang-format on
 
 namespace py = pybind11;
 
@@ -23,14 +25,14 @@ namespace initializer
 
 
 
-    class __attribute__ ((visibility ("hidden")))  PythonDataProvider : public DataProvider
+    class __attribute__((visibility("hidden"))) PythonDataProvider : public DataProvider
     {
     public:
         PythonDataProvider(int argc, char const* argv)
         {
             if (argc == 2)
             {
-                initModuleName_ = argv[1];
+                initModuleName_ = std::string{argv};
             }
             preparePythonPath_();
         }
@@ -42,14 +44,14 @@ namespace initializer
         virtual void read() override
         {
             py::eval_file(initModuleName_, scope_);
-            auto& d                      = dict<1>();
+            /*auto& d = dict<1>();
             auto& pop1                   = d["simulation"]["hybridstate"]["ions"]["pop1"];
             auto popname                 = pop1["name"].to<std::string>();
             auto particleInitializerName = pop1["particleinitializer"]["name"].to<std::string>();
             auto& density = pop1["particleinitializer"]["density"].to<ScalarFunction<1>>();
             std::cout << "pop1 (" + popname + ") has a " + particleInitializerName
                              + "particle initializer"
-                      << " and density(2) = " << density(2.) << "\n";
+                      << " and density(2) = " << density(2.) << "\n";*/
         }
 
 
