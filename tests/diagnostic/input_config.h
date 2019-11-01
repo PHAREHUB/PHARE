d27d75d2644a2becce9e8ef55f4e6f51d3e040c4
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2019-11-01 18:23:00 +0100

    compiles and one test fail (refineGhosts)

---
+++ tests/diagnostic/input_config.h
@@ -0,0 +1,11 @@
+#ifndef TESTS_REFINE_INPUT_CONFIG_H
+#define TESTS_REFINE_INPUT_CONFIG_H
+
+#include <iostream>
+#include <string>
+
+
+const std::string inputBase = "/home/philix/git/phare/diagnostics/tests/diagnostic/";
+
+
+#endif
