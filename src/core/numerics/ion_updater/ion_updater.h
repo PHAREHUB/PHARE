35abdbdef6d297a338d7ae5914a2a5b10f565230
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2020-01-16 14:21:50 +0100

    create IonUpdater and assert 100 part. per cell are created in domain

---
+++ src/core/numerics/ion_updater/ion_updater.h
@@ -0,0 +1,32 @@
+#ifndef PHARE_ION_UPDATER_H
+#define PHARE_ION_UPDATER_H
+
+namespace PHARE
+{
+namespace core
+{
+    enum class UpdaterMode { moments_only = 1, particles_and_moments = 2 };
+
+    class IonUpdater
+    {
+    public:
+        template<typename Ions, typename Electromag, typename GridLayout>
+        void update(Ions& ions, Electromag const& em, GridLayout const& layout,
+                    UpdaterMode = UpdaterMode::particles_and_moments);
+    };
+
+
+    template<typename Ions, typename Electromag, typename GridLayout>
+    void IonUpdater::update(Ions& ions, Electromag const& em, GridLayout const& layout,
+                            UpdaterMode mode)
+    {
+        //
+    }
+
+
+
+} // namespace core
+} // namespace PHARE
+
+
+#endif // ION_UPDATER_H
