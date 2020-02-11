431531bfe1c10db83017c111c93ade786c6b820f
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2020-02-11 13:48:20 +0100

    add setLayout and LayoutHolder and refactors faraday

---
+++ src/core/data/grid/gridlayout_utils.h
@@ -0,0 +1,53 @@
+#ifndef PHARE_GRIDLAYOUT_UTILS_H
+#define PHARE_GRIDLAYOUT_UTILS_H
+
+#include <tuple>
+#include <stdexcept>
+
+
+template<typename GridLayout>
+class LayoutHolder
+{
+protected:
+    GridLayout* layout_{nullptr};
+
+public:
+    void setLayout(GridLayout* ptr)
+    {
+        if (!hasLayout())
+            layout_ = ptr;
+        else
+            throw std::runtime_error("Error - layout already set, cannot set layout");
+    }
+
+    bool hasLayout() const { return layout_ != nullptr; }
+};
+
+
+
+
+template<typename GridLayout, typename... GridLayoutSettable>
+class SetLayout
+{
+public:
+    SetLayout(GridLayout* ptr, GridLayoutSettable&... settables)
+        : settables_{settables...}
+    {
+        std::apply([this, ptr](auto&... settable) { (settable.setLayout(nullptr), ...); },
+                   settables_);
+    }
+
+    ~SetLayout()
+    {
+        std::apply([this](auto&... settable) { (settable.setLayout(nullptr), ...); }, settables_);
+    }
+
+
+private:
+    std::tuple<GridLayoutSettable&...> settables_;
+};
+
+
+
+
+#endif
--- src/core/numerics/faraday/faraday.h
+++ src/core/numerics/faraday/faraday.h
@@ -5,6 +5,7 @@
 #include <iostream>
 
 #include "core/data/grid/gridlayoutdefs.h"
+#include "core/data/grid/gridlayout_utils.h"
 #include "core/data/vecfield/vecfield_component.h"
 #include "core/utilities/index/index.h"
 
@@ -12,67 +13,11 @@ namespace PHARE
 {
 namespace core
 {
-    /** @brief Actual implementation of the Faraday equation
-     *
-     * This implementation is used by the Faraday object to calculate the magentic field
-     * It is templated by the layout and the dimension so specialized template actually do the job
-     *
-     */
-    template<typename GridLayout, std::size_t dim>
-    class FaradayImpl
-    {
-    };
-
-
-    /** @brief Base class for FaradayImpl specialized classes
-     * to factorize the code that deals with the GridLayout.
-     *
-     * The purpose of this class is to store the GridLayout
-     * pointer of Faraday, and propose the interface to check
-     * whether it's been set or not.
-     *
-     */
     template<typename GridLayout>
-    class FaradayImplInternals
+    class Faraday : public LayoutHolder<GridLayout>
     {
-    protected:
-        GridLayout* layout_{nullptr};
-
-    public:
-        /**
-         * @brief hasLayoutSet returns true if the Layout has been set
-         */
-        bool hasLayoutSet() const { return (layout_ == nullptr) ? false : true; }
-
-
-        /**
-         * @brief setLayout is used to give Ampere a pointer to a gridlayout
-         */
-        void setLayout(GridLayout* layout)
-        {
-            if (layout_ != nullptr)
-            {
-                throw std::runtime_error(
-                    "Error - ampere - cannot set layout_ because it is already set");
-            }
-            else
-            {
-                layout_ = layout;
-            }
-        }
-    };
-
-
-    /** @brief 1D specialization of the ampere equation solver implementation
-     */
-    template<typename GridLayout>
-    class FaradayImpl<GridLayout, 1> : public FaradayImplInternals<GridLayout>
-    {
-        static_assert(GridLayout::dimension == 1,
-                      "Error: Passed non-1D GridLayout to 1D FaradayImpl");
-
-    public:
-        template<typename VecField>
+    private:
+        template<typename VecField, std::size_t dim, std::enable_if_t<dim == 1, int> = 0>
         void operator()(VecField const& B, VecField const& E, VecField& Bnew, double dt)
         {
             // dBxdt =  0
@@ -107,18 +52,10 @@ namespace core
                     = Bz(ix) - dt * this->layout_->deriv(Ey, {ix}, DirectionTag<Direction::X>{});
             }
         }
-    };
 
 
 
-    template<typename GridLayout>
-    class FaradayImpl<GridLayout, 2> : public FaradayImplInternals<GridLayout>
-    {
-        static_assert(GridLayout::dimension == 2,
-                      "Error: Passed non-2D GridLayout to 2D FaradayImpl");
-
-    public:
-        template<typename VecField>
+        template<typename VecField, std::size_t dim, std::enable_if_t<dim == 2, int> = 0>
         void operator()(VecField const& B, VecField const& E, VecField& Bnew, double dt)
         {
             // dBxdt =  -dyEz
@@ -183,18 +120,10 @@ namespace core
                 }
             }
         }
-    };
 
 
 
-    template<typename GridLayout>
-    class FaradayImpl<GridLayout, 3> : public FaradayImplInternals<GridLayout>
-    {
-        static_assert(GridLayout::dimension == 3,
-                      "Error: Passed non-3D GridLayout to 3D FaradayImpl");
-
-    public:
-        template<typename VecField>
+        template<typename VecField, std::size_t dim, std::enable_if_t<dim == 3, int> = 0>
         void operator()(VecField const& B, VecField const& E, VecField& Bnew, double dt)
         {
             // dBxdt = -dyEz + dzEy
@@ -288,32 +217,20 @@ namespace core
                 }
             }
         }
-    };
 
-
-
-
-    template<typename GridLayout>
-    class Faraday
-    {
-    private:
-        FaradayImpl<GridLayout, GridLayout::dimension> impl_;
 
     public:
         template<typename VecField>
         void operator()(VecField const& B, VecField const& E, VecField& Bnew, double dt)
         {
-            if (!impl_.hasLayoutSet())
+            if (!this->hasLayout())
             {
                 throw std::runtime_error(
                     "Error - Faraday - GridLayout not set, cannot proceed to calculate faraday()");
             }
 
-            impl_(B, E, Bnew, dt);
+            this->operator()<VecField, GridLayout::dimension>(B, E, Bnew, dt);
         }
-
-
-        void setLayout(GridLayout* layout) { impl_.setLayout(layout); }
     };
 } // namespace core
 } // namespace PHARE
