201dae88d6eff2eca24e1a424c80d6328608d496
dekken <philip.deegan@gmail.com>
2020-03-15 22:05:49 +0100

    :(

--- res/cmake/highfive.cmake
+++ res/cmake/highfive.cmake
@@ -4,13 +4,23 @@ if(HighFive)
     ${CMAKE_CURRENT_SOURCE_DIR}/subprojects/highfive/include
   )
 
+  message("HighFive enabled - checking HDF5")
+
   if(DEFINED HDF5_ROOT)
     find_package(HDF5 PATHS ${HDF5_ROOT} REQUIRED)
   else()
     find_package(HDF5 REQUIRED)
   endif()
 
-  message("HighFive enabled - checking HDF5")
+  if(DEFINED HDF5_ENABLE_PARALLEL AND "${HDF5_ENABLE_PARALLEL}" STREQUAL "ON")
+    set (HDF5_IS_PARALLEL TRUE)
+
+    # This is here for HDF5 versions > 1.12
+    #  This should be able to be removed when this is merged to master
+    #   https://github.com/BlueBrain/HighFive/pull/311
+    set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DH5Oget_info_vers=1 -DH5O_info_t_vers=1")
+  endif()
+
   if(${HDF5_IS_PARALLEL})
       message("HDF5 PARALLEL detected")
       add_definitions(-DH5_HAVE_PARALLEL)
