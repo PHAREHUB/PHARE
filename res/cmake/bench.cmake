b66158fd2e706d515cb04cd69e962c265d19f989
dekken <philip.deegan@gmail.com>
2020-09-29 21:29:17 +0200

    cmake:  exec_level to phare_cpp_benchmark function to disable default activation

--- res/cmake/bench.cmake
+++ res/cmake/bench.cmake
@@ -1,9 +1,9 @@
 
 # public test functions
 #
-#   add_phare_benchmark(project file directory)
+#   add_phare_cpp_benchmark(exec_level project file directory)
 #    create $target ${project}_${file}
-#    execute $target in $directory, with mpirun when -DtestMPI=ON
+#    execute $target in $directory, with mpirun when -DtestMPI=ON if exec_level is active
 #
 
 if (bench)
@@ -30,18 +30,18 @@ if (bench)
 
   endif()
 
-  function(add_phare_benchmark_ target file directory)
+  function(add_phare_cpp_benchmark_ exec_level target file directory)
     add_executable(${target} ${file})
     target_compile_options(${target} PRIVATE ${PHARE_WERROR_FLAGS} -DPHARE_HAS_HIGHFIVE=${PHARE_HAS_HIGHFIVE})
     set_property(TARGET ${target} PROPERTY INTERPROCEDURAL_OPTIMIZATION ${PHARE_INTERPROCEDURAL_OPTIMIZATION})
     target_include_directories(${target} PUBLIC ${PHARE_PROJECT_DIR}/subprojects/googlebench/include)
     add_phare_test(${target} ${directory}) # using this function means benchmarks can be run with MPI, not sure this is good.
-  endfunction(add_phare_benchmark_)
+  endfunction(add_phare_cpp_benchmark_)
 
-  function(add_phare_benchmark project file directory)
-    add_phare_benchmark_(${project}_${file} ${file}.cpp ${directory})
+  function(add_phare_cpp_benchmark exec_level project file directory)
+    add_phare_cpp_benchmark_(${project}_${file} ${file}.cpp ${directory})
     target_link_libraries(${project}_${file} PUBLIC ${GBENCH_LIBS} phare_simulator)
-  endfunction(add_phare_benchmark)
+  endfunction(add_phare_cpp_benchmark)
 
   add_subdirectory(tools/bench/core/data/particles)
   add_subdirectory(tools/bench/hi5)
