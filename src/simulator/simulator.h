d2e3769bd12386235510ecbb34c3ca3e6b379db6
dekken <philip.deegan@gmail.com>
2021-06-04 14:55:51 +0200

    better

--- src/simulator/simulator.h
+++ src/simulator/simulator.h
@@ -124,7 +124,6 @@ private:
     }
 
     std::shared_ptr<MultiPhysicsIntegrator> multiphysInteg_{nullptr};
-    std::exception_ptr exception_ptr_{nullptr};
 };
 
 
@@ -282,18 +281,15 @@ double Simulator<_dimension, _interp_order, _nbRefinedPart>::advance(double dt)
     catch (std::runtime_error const& e)
     {
         std::cerr << "EXCEPTION CAUGHT: " << e.what() << std::endl;
-        exception_ptr_ = std::current_exception();
-        stop           = true;
+        core::mpi::Errors::I()(std::current_exception());
+        stop = true;
     }
     catch (...)
     {
         std::cerr << "UNKNOWN EXCEPTION CAUGHT" << std::endl;
-        exception_ptr_ = std::current_exception();
-        stop           = true;
+        core::mpi::Errors::I()(std::current_exception());
+        stop = true;
     }
-
-    if (exception_ptr_)
-        core::mpi::Errors::I()(exception_ptr_);
 
     if (stop)
     {
