07362279ab31f431b98f72f3130cf960f6cc5779
roch smets <roch.smets@gmail.com>
2022-04-19 17:11:54 +0200

    wip

--- src/amr/work_load/workload.hpp
+++ src/amr/work_load/workload.hpp
@@ -13,17 +13,22 @@
 
 namespace PHARE::amr
 {
-template<std::size_t dim>
+template<typename PHARE_T>
 class IWorkLoadEstimator
 {
+    static constexpr std::string workLoadName{"workLoad_" + PHARE_T::dimension + "_"
+                                              + PHARE_T::interp_order + "_"
+                                              + PHARE_T::nbRefinedPart};
+
 public:
     IWorkLoadEstimator()
         : variableDatabase_{SAMRAI::hier::VariableDatabase::getDatabase()}
-        , dimension_{SAMRAI::tbox::Dimension{dim}}
-        , workLoadVariable_{std::make_shared<SAMRAI::pdat::CellVariable<double>>(dimension_,
-                                                                                 "workLoad")}
+        , dimension_{SAMRAI::tbox::Dimension{PHARE_T::dim}}
+        , workLoadVariable_{std::make_shared<SAMRAI::pdat::CellVariable<double>>(
+              dimension_, workLoadName)} // TODO make a name with dim, iterporder, nbr_refine
         , context_{variableDatabase_->getContext("default")}
-    // , id_{variableDatabase_->registerVariableAndContext(workLoadVariable_, context_, gw0)}
+        , id_{variableDatabase_->registerVariableAndContext(
+              workLoadVariable_, context_, SAMRAI::hier::IntVector::getZero(dimension_))}
     {
         // TODO have the correct initialization in the ctor above
     }
@@ -35,17 +40,20 @@ public:
     int getID() { return id_; };
     virtual std::string name() const = 0;
 
-protected:
-    int const id_{12}; // TODO to be initialized in the ctor
-
 private:
     SAMRAI::hier::VariableDatabase* variableDatabase_;
     SAMRAI::tbox::Dimension dimension_;
-    // std::shared_ptr<SAMRAI::pdat::CellData<double>> workLoad_;
+    // std::shared_ptr<SAMRAI::pdat::CellData<double>> workLoad_; now this var is locally defined in
+    // advanceLevel
     std::shared_ptr<SAMRAI::pdat::CellVariable<double>> workLoadVariable_;
-    // std::string contextName_{"default"};
+    // std::string contextName_{"default"}; directly called in the init list of the ctor
     std::shared_ptr<SAMRAI::hier::VariableContext> context_;
-    SAMRAI::hier::IntVector gw0 = SAMRAI::hier::IntVector::getZero(dimension_);
+    // SAMRAI::hier::IntVector gw0 = SAMRAI::hier::IntVector::getZero(dimension_); also directly
+    // called i, the init list of the ctor
+
+protected:
+    int const id_;
+    // int const id_{12 };   // TODO to be initialized in the ctor
 };
 
 } // namespace PHARE::amr
