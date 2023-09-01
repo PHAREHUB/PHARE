688a803617be8a426aa4e0ccf5f10b94c4f14755
Nicolas Aunai <nicolas.aunai@lpp.polytechnique.fr>
2023-09-01 18:34:28 +0200

    wip

--- tests/core/utilities/sorting/test_sorting.cpp
+++ tests/core/utilities/sorting/test_sorting.cpp
@@ -15,16 +15,6 @@
 
 using namespace PHARE::core;
 
-template<typename T, size_t N>
-constexpr T array_multiply(const std::array<T, N>& arr)
-{
-    T result = 1;
-    for (const T& element : arr)
-    {
-        result *= element;
-    }
-    return result;
-}
 
 template<typename Dim>
 class SortingTest : public ::testing::Test
@@ -32,7 +22,7 @@ class SortingTest : public ::testing::Test
 public:
     std::size_t constexpr static dim                    = Dim::value;
     std::array<int, dim> constexpr static lastCellIndex = ConstArray<int, dim>(100);
-    std::size_t constexpr static nbrParticles           = 100 * array_multiply(lastCellIndex);
+    std::size_t constexpr static nbrParticles           = 100 * product(lastCellIndex);
 
     SortingTest()
         : box{ConstArray<int, dim>(0), lastCellIndex}
@@ -41,6 +31,14 @@ public:
         std::random_device rd;
         std::mt19937_64 gen(rd());
         std::uniform_int_distribution<int> dist(0, lastCellIndex[0]);
+
+        for (std::size_t iPart = 0; iPart < nbrParticles; ++iPart)
+        {
+            for (std::size_t iDim = 0; iDim < dim; ++iDim)
+            {
+                particles[iPart].iCell[iDim] = dist(gen);
+            }
+        }
     }
 
     Box<int, dim> box;
@@ -56,18 +54,15 @@ TYPED_TEST_SUITE(SortingTest, ConcreteSortingTests);
 
 TYPED_TEST(SortingTest, CoutingSort)
 {
-    int const nbrParticles = 1000;
-    Box<int, 1> box{{0}, {99}};
-    ParticleArray<1> particles(box, nbrParticles);
-    CountingSort<ParticleArray<1>, 1> sorter;
+    CountingSort<ParticleArray<TypeParam::value>, TypeParam::value> sorter;
 
     // when allocation is ok find a way to allocate room enough for
     // multiple sorting
-    sorter.set_size(nbrParticles);
+    sorter.set_size(this->nbrParticles);
 
     // when ready to sort
-    auto cellFattener = [](auto const& particle) { return particle.iCell[0]; };
-    sorter.sort(particles, cellFattener);
+    // auto cellFattener = [](auto const& particle) { return particle.iCell[0]; };
+    // sorter.sort(particles, cellFattener);
 }
 
 
