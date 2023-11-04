1a7a04bcc46c4f0b734b79f6cf038b61127ba943
PhilipDeegan <philip.deegan@gmail.com>
2023-11-04 11:56:03 +0100

    Refactor some tests to be only run under serial execution (#376)
    * AMR Field Refine: extract some tests to be only run under serial execution
    * AMR Field Overlap: refactor tests to be only run under serial execution
    * AMR Field Time Interpolate: refactor tests to be only run under serial execution
    * AMR: rm tests from MPI suite

--- tests/amr/data/field/refine/test_field_refinement_on_hierarchy.cpp
+++ tests/amr/data/field/refine/test_field_refinement_on_hierarchy.cpp
@@ -129,3 +129,24 @@ TYPED_TEST(ALinearFieldRefineTest, ConserveLinearFunction)
         }
     }
 }
+
+
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    SAMRAI::tbox::SAMRAI_MPI::init(&argc, &argv);
+    SAMRAI::tbox::SAMRAIManager::initialize();
+    SAMRAI::tbox::SAMRAIManager::startup();
+
+
+    int testResult = RUN_ALL_TESTS();
+
+    // Finalize
+    SAMRAI::tbox::SAMRAIManager::shutdown();
+    SAMRAI::tbox::SAMRAIManager::finalize();
+    SAMRAI::tbox::SAMRAI_MPI::finalize();
+
+    return testResult;
+}
