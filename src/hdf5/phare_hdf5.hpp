64bd04637ed625fd45ad7777c54602baf756df4a
dekken <philip.deegan@gmail.com>
2024-02-19 13:18:08 +0100

    highfive version correction

--- src/hdf5/phare_hdf5.hpp
+++ src/hdf5/phare_hdf5.hpp
@@ -4,6 +4,13 @@
 
 #if PHARE_HAS_HIGHFIVE
 
+#include "highfive/H5Version.hpp"
+
+#ifndef HIGHFIVE_VERSION_STRING
+#pragma message("Highfive must be at least version 2.7.1")
+#error // highfive is too old
+#endif
+
 #define _PHARE_WITH_HIGHFIVE(...) __VA_ARGS__
 #else
 #define _PHARE_WITH_HIGHFIVE(...)
