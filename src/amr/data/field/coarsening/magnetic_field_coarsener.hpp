58a14e9d38769b30b9f788a113f71858edaadf9f
deegan <philip.deegan@gmail.com>
2024-03-05 14:02:28 +0100

    separate magnetic coarsening op into per dim fns

--- src/amr/data/field/coarsening/magnetic_field_coarsener.hpp
+++ src/amr/data/field/coarsening/magnetic_field_coarsener.hpp
@@ -31,110 +31,134 @@ using core::dirZ;
  * of the enclosed fine faces
  *
  */
-template<std::size_t dimension>
+template<std::size_t dim>
 class MagneticFieldCoarsener
 {
+    using Point_t = core::Point<int, dim>;
+
 public:
-    MagneticFieldCoarsener(std::array<core::QtyCentering, dimension> const centering,
+    MagneticFieldCoarsener(std::array<core::QtyCentering, dim> const centering,
                            SAMRAI::hier::Box const& sourceBox,
                            SAMRAI::hier::Box const& destinationBox,
                            SAMRAI::hier::IntVector const& ratio)
         : centering_{centering}
         , sourceBox_{sourceBox}
         , destinationBox_{destinationBox}
-
     {
     }
 
+
     template<typename FieldT>
-    void operator()(FieldT const& fineField, FieldT& coarseField,
-                    core::Point<int, dimension> coarseIndex)
+    void operator()(FieldT const& fineField, FieldT& coarseField, core::Point<int, dim> coarseIndex)
     {
         // For the moment we only take the case of field with the same centering
         TBOX_ASSERT(fineField.physicalQuantity() == coarseField.physicalQuantity());
 
-        core::Point<int, dimension> fineStartIndex;
+        coarsen<dim>(fine_start_index(coarseIndex), fineField, coarseField,
+                     AMRToLocal(coarseIndex, destinationBox_));
+    }
 
+private:
+    auto fine_start_index(core::Point<int, dim> const coarseIndex) const
+    {
+        core::Point<int, dim> fineStartIndex;
         fineStartIndex[dirX] = coarseIndex[dirX] * this->ratio_;
-
-        if constexpr (dimension > 1)
+        if constexpr (dim > 1)
         {
             fineStartIndex[dirY] = coarseIndex[dirY] * this->ratio_;
-            if constexpr (dimension > 2)
+            if constexpr (dim > 2)
             {
                 fineStartIndex[dirZ] = coarseIndex[dirZ] * this->ratio_;
             }
         }
-
-        fineStartIndex = AMRToLocal(fineStartIndex, sourceBox_);
-        coarseIndex    = AMRToLocal(coarseIndex, destinationBox_);
-
-        // the following kinda assumes where B is, i.e. Yee layout centering
-        // as it only does faces pirmal-dual, dual-primal and dual-dual
-
-        if constexpr (dimension == 1)
-        {
-            // in 1D div(B) is automatically satisfied so using this coarsening
-            // opertor is probably not better than the default one, but we do that
-            // for a kind of consistency...
-            // coarse flux is equal to fine flux and we're 1D so there is flux partitioned
-            // only for By and Bz, Bx is equal to the fine value
-
-            if (centering_[dirX] == core::QtyCentering::primal) // bx
-            {
-                coarseField(coarseIndex[dirX]) = fineField(fineStartIndex[dirX]);
-            }
-            else if (centering_[dirX] == core::QtyCentering::dual) // by and bz
-            {
-                coarseField(coarseIndex[dirX])
-                    = 0.5 * (fineField(fineStartIndex[dirX] + 1) + fineField(fineStartIndex[dirX]));
-            }
-        }
-
-        if constexpr (dimension == 2)
-        {
-            if (centering_[dirX] == core::QtyCentering::primal
-                and centering_[dirY] == core::QtyCentering::dual)
-            {
-                coarseField(coarseIndex[dirX], coarseIndex[dirY])
-                    = 0.5
-                      * (fineField(fineStartIndex[dirX], fineStartIndex[dirY])
-                         + fineField(fineStartIndex[dirX], fineStartIndex[dirY] + 1));
-            }
-            else if (centering_[dirX] == core::QtyCentering::dual
-                     and centering_[dirY] == core::QtyCentering::primal)
-            {
-                coarseField(coarseIndex[dirX], coarseIndex[dirY])
-                    = 0.5
-                      * (fineField(fineStartIndex[dirX], fineStartIndex[dirY])
-                         + fineField(fineStartIndex[dirX] + 1, fineStartIndex[dirY]));
-            }
-            else if (centering_[dirX] == core::QtyCentering::dual
-                     and centering_[dirY] == core::QtyCentering::dual)
-            {
-                coarseField(coarseIndex[dirX], coarseIndex[dirY])
-                    = 0.25
-                      * (fineField(fineStartIndex[dirX], fineStartIndex[dirY])
-                         + fineField(fineStartIndex[dirX] + 1, fineStartIndex[dirY])
-                         + fineField(fineStartIndex[dirX], fineStartIndex[dirY] + 1)
-                         + fineField(fineStartIndex[dirX] + 1, fineStartIndex[dirY] + 1));
-            }
-            else
-            {
-                throw std::runtime_error("no magnetic field should end up here");
-            }
-        }
-        else if constexpr (dimension == 3)
-        {
-            throw std::runtime_error("Not Implemented yet");
-        }
+        return AMRToLocal(fineStartIndex, sourceBox_);
     }
 
-private:
-    std::array<core::QtyCentering, dimension> const centering_;
+
+    template<std::size_t D, typename FieldT>
+    typename std::enable_if<D == 1, void>::type
+    coarsen(Point_t const fineStartIndex, FieldT const& fineField, FieldT& coarseField,
+            Point_t const coarseIndex);
+
+    template<std::size_t D, typename FieldT>
+    typename std::enable_if<D == 2, void>::type
+    coarsen(Point_t const fineStartIndex, FieldT const& fineField, FieldT& coarseField,
+            Point_t const coarseIndex);
+    template<std::size_t D, typename FieldT>
+    typename std::enable_if<D == 3, void>::type
+    coarsen(Point_t const fineStartIndex, FieldT const& fineField, FieldT& coarseField,
+            Point_t const coarseIndex);
+
+
+    std::array<core::QtyCentering, dim> const centering_;
     SAMRAI::hier::Box const sourceBox_;
     SAMRAI::hier::Box const destinationBox_;
     static int constexpr ratio_ = 2;
 };
+
+template<std::size_t dim>
+template<std::size_t D, typename FieldT>
+typename std::enable_if<D == 1, void>::type
+MagneticFieldCoarsener<dim>::coarsen(Point_t const fineStartIndex, FieldT const& fineField,
+                                     FieldT& coarseField, Point_t const coarseIndex)
+{
+    if (centering_[dirX] == core::QtyCentering::primal) // bx
+    {
+        coarseField(coarseIndex[dirX]) = fineField(fineStartIndex[dirX]);
+    }
+    else if (centering_[dirX] == core::QtyCentering::dual) // by and bz
+    {
+        coarseField(coarseIndex[dirX])
+            = 0.5 * (fineField(fineStartIndex[dirX] + 1) + fineField(fineStartIndex[dirX]));
+    }
+}
+
+template<std::size_t dim>
+template<std::size_t D, typename FieldT>
+typename std::enable_if<D == 2, void>::type
+MagneticFieldCoarsener<dim>::coarsen(Point_t const fineStartIndex, FieldT const& fineField,
+                                     FieldT& coarseField, Point_t const coarseIndex)
+{
+    if (centering_[dirX] == core::QtyCentering::primal
+        and centering_[dirY] == core::QtyCentering::dual)
+    {
+        coarseField(coarseIndex[dirX], coarseIndex[dirY])
+            = 0.5
+              * (fineField(fineStartIndex[dirX], fineStartIndex[dirY])
+                 + fineField(fineStartIndex[dirX], fineStartIndex[dirY] + 1));
+    }
+    else if (centering_[dirX] == core::QtyCentering::dual
+             and centering_[dirY] == core::QtyCentering::primal)
+    {
+        coarseField(coarseIndex[dirX], coarseIndex[dirY])
+            = 0.5
+              * (fineField(fineStartIndex[dirX], fineStartIndex[dirY])
+                 + fineField(fineStartIndex[dirX] + 1, fineStartIndex[dirY]));
+    }
+    else if (centering_[dirX] == core::QtyCentering::dual
+             and centering_[dirY] == core::QtyCentering::dual)
+    {
+        coarseField(coarseIndex[dirX], coarseIndex[dirY])
+            = 0.25
+              * (fineField(fineStartIndex[dirX], fineStartIndex[dirY])
+                 + fineField(fineStartIndex[dirX] + 1, fineStartIndex[dirY])
+                 + fineField(fineStartIndex[dirX], fineStartIndex[dirY] + 1)
+                 + fineField(fineStartIndex[dirX] + 1, fineStartIndex[dirY] + 1));
+    }
+    else
+    {
+        throw std::runtime_error("no magnetic field should end up here");
+    }
+}
+
+template<std::size_t dim>
+template<std::size_t D, typename FieldT>
+typename std::enable_if<D == 3, void>::type
+MagneticFieldCoarsener<dim>::coarsen(Point_t const fineStartIndex, FieldT const& fineField,
+                                     FieldT& coarseField, Point_t const coarseIndex)
+{
+    throw std::runtime_error("Not Implemented yet");
+}
+
 } // namespace PHARE::amr
 #endif
