73461660912a677bc763abea0b3c1bed0d0dd54b
dekken <philip.deegan@gmail.com>
2024-04-19 21:11:34 +0200

    gcc11 doesn't support all param flags

--- res/cmake/def.cmake
+++ res/cmake/def.cmake
@@ -3,14 +3,20 @@
 # Per compiler CXXFLAGS
 set (PHARE_FLAGS ${PHARE_FLAGS} )
 # mostly for silencing spurious Werrors
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-  # --param=min-pagesize=0    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105523
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")  u
   unset(CHECK_SUPPORTS_FLAG CACHE)
+  # --param=min-pagesize=0    https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105523
   check_cxx_compiler_flag( --param=min-pagesize=0 CHECK_SUPPORTS_FLAG)
   if (${CHECK_SUPPORTS_FLAG})
     set (PHARE_FLAGS ${PHARE_FLAGS} --param=min-pagesize=0 )
   endif()
+
   unset(CHECK_SUPPORTS_FLAG CACHE)
+  # --param=evrp-mode=legacy  https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105329
+  check_cxx_compiler_flag( --param=evrp-mode=legacy CHECK_SUPPORTS_FLAG)
+  if (${CHECK_SUPPORTS_FLAG})
+    set (PHARE_FLAGS ${PHARE_FLAGS} --param=evrp-mode=legacy )
+  endif()
 endif() # compiler is GNU
 
 set (PHARE_LINK_FLAGS )
