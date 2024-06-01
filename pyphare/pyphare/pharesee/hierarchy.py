6c9f1f2e951448786355dd62a561703c0c9f6a4d
roch smets <roch.smets@gmail.com>
2024-06-01 16:21:49 +0200

    up

--- pyphare/pyphare/pharesee/hierarchy.py
+++ pyphare/pyphare/pharesee/hierarchy.py
@@ -1157,31 +1157,133 @@ class ScalarField(PatchHierarchy):
 
         super().__init__(patch_levels, domain_box, refinement_ratio, renamed_hier.times(), data_files)
 
+    def __add__(self, other):
+        assert isinstance(other, (ScalarField, int, float))
+        h_self = rename(self, ["self_value"])
+
+        if isinstance(other, ScalarField):
+            h_other = rename(other, ["other_value"])
+            h = compute_hier_from(self._compute_add, (h_self, h_other),)
+        elif isinstance(other, (int, float)):
+            h = compute_hier_from(self._compute_add, (h_self,), other=other)
+            pass
+        else:
+            raise RuntimeError("right operand not supported")
+
+        return ScalarField(h)
+
+    def __sub__(self, other):
+        assert isinstance(other, (ScalarField, int, float))
+        h_self = rename(self, ["self_value"])
+
+        if isinstance(other, ScalarField):
+            h_other = rename(other, ["other_value"])
+            h = compute_hier_from(self._compute_sub, (h_self, h_other),)
+        elif isinstance(other, (int, float)):
+            h = compute_hier_from(self._compute_sub, (h_self,), other=other)
+        else:
+            raise RuntimeError("right operand not supported")
+
+        return ScalarField(h)
+
     def __mul__(self, other):
-        assert isinstance(other, (int, float))
-        h = compute_hier_from(_compute_mul, self, names=("value",), other=other)
+        assert isinstance(other, (ScalarField, int, float))
+        h_self = rename(self, ["self_value"])
+
+        if isinstance(other, ScalarField):
+            h_other = rename(other, ["other_value"])
+            h = compute_hier_from(self._compute_mul, (h_self, h_other))
+        elif isinstance(other, (int, float)):
+            h = compute_hier_from(self._compute_mul, (h_self,), other=other)
+        else:
+            raise RuntimeError("right operand not supported")
+
         return ScalarField(h)
 
     def __rmul__(self, other):
         return self.__mul__(other)
 
-    def __add__(self, other):
-        assert isinstance(other, ScalarField)
+    def __truediv__(self, other):
+        assert isinstance(other, (ScalarField, int, float))
         h_self = rename(self, ["self_value"])
-        h_other = rename(other, ["other_value"])
-        h = compute_hier_from(self._compute_add, (h_self, h_other),)
+
+        if isinstance(other, ScalarField):
+            h_other = rename(other, ["other_value"])
+            h = compute_hier_from(self._compute_truediv, (h_self, h_other))
+        elif isinstance(other, (int, float)):
+            h = compute_hier_from(self._compute_truediv, (h_self,), other=other)
+        else:
+            raise RuntimeError("right operand not supported")
+
         return ScalarField(h)
 
     def _compute_add(self, patch_datas, **kwargs):
         ref_name = next(iter(patch_datas.keys()))
 
-        dset_value = patch_datas["self_value"].dataset[:] +\
-            patch_datas["other_value"].dataset[:]
+        if "other" in kwargs:
+            other = kwargs["other"]
+            dset_value = patch_datas["self_value"].dataset[:] + other
+        else:
+            dset_value = patch_datas["self_value"].dataset[:] +\
+                patch_datas["other_value"].dataset[:]
 
         return (
             {"name": "value", "data": dset_value, "centering": patch_datas[ref_name].centerings},
         )
 
+    def _compute_sub(self, patch_datas, **kwargs):
+        # subtract a scalar from the dataset of a scalarField
+        if "other" in kwargs:
+            other = kwargs["other"]
+            dset_value = patch_datas["self_value"].dataset[:] - other
+        # substraction of 2 scalarFields
+        else:
+            dset_value = patch_datas["self_value"].dataset[:] -\
+                patch_datas["other_value"].dataset[:]
+
+        return ({"name": "value",
+                 "data": dset_value,
+                 "centering": patch_datas["self_value"].centerings},)
+
+    def _compute_mul(self, patch_datas, **kwargs):
+        # multiplication of a scalarField by a scalar
+        if "other" in kwargs:
+            other = kwargs["other"]
+            pd_attrs = []
+
+            for pd_name in patch_datas:
+                pd_attrs.append({"name": "value",
+                                 "data": other * patch_datas[pd_name].dataset[:],
+                                 "centering": patch_datas[pd_name].centerings,})
+        # multiplication of 2 scalarField
+        else:
+            dset_value = patch_datas["self_value"].dataset[:] *\
+                         patch_datas["other_value"].dataset[:]
+            pd_attrs = ({"name": "value",
+                         "data": dset_value,
+                         "centering": patch_datas["self_value"].centerings},)
+
+        return tuple(pd_attrs)
+
+    def _compute_truediv(self, patch_datas, **kwargs):
+        # multiplication of a scalarField by a scalar
+        if "other" in kwargs:
+            other = kwargs["other"]
+            pd_attrs = []
+
+            for pd_name in patch_datas:
+                pd_attrs.append({"name": "value",
+                                 "data": patch_datas[pd_name].dataset[:] / other,
+                                 "centering": patch_datas[pd_name].centerings,})
+        # multiplication of 2 scalarField
+        else:
+            dset_value = patch_datas["self_value"].dataset[:] /\
+                         patch_datas["other_value"].dataset[:]
+            pd_attrs = ({"name": "value",
+                         "data": dset_value,
+                         "centering": patch_datas["self_value"].centerings},)
+
+        return tuple(pd_attrs)
 
 
 class VectorField(PatchHierarchy):
@@ -1263,17 +1365,23 @@ class VectorField(PatchHierarchy):
 
 def _compute_mul(patch_datas, **kwargs):
     names = kwargs["names"]
-    other = kwargs["other"]
-    pd_attrs = []
 
-    for name, pd_name in zip(names, patch_datas):
-        pd_attrs.append(
-            {
-                "name": name,
-                "data": other * patch_datas[pd_name].dataset[:],
-                "centering": patch_datas[pd_name].centerings,
-            }
-        )
+    # multiplication of a scalarField or VectorField by a scalar
+    if "other" in kwargs:
+        other = kwargs["other"]
+        pd_attrs = []
+
+        for name, pd_name in zip(names, patch_datas):
+            pd_attrs.append({"name": name,
+                             "data": other * patch_datas[pd_name].dataset[:],
+                             "centering": patch_datas[pd_name].centerings, })
+    # multiplication of 2 scalarField
+    elif "self_value" in patch_datas:
+        dset_value = patch_datas["self_value"].dataset[:] *\
+                     patch_datas["other_value"].dataset[:]
+        pd_attrs = ({"name": "value",
+                     "data": dset_value,
+                     "centering": patch_datas["self_value"].centerings},)
 
     return tuple(pd_attrs)
 
