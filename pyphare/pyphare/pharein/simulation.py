3bd22e1d5503b99a97db288041ff270aaa643c01
PhilipDeegan <philip.deegan@gmail.com>
2024-07-22 19:56:02 +0200

    rebase / torms

--- pyphare/pyphare/pharein/simulation.py
+++ pyphare/pyphare/pharein/simulation.py
@@ -581,16 +581,6 @@ def check_clustering(**kwargs):
     return clustering
 
 
-def check_loadbalancing(**kwargs):
-    valid_keys = ["nppc", "homogeneous"]
-    loadbalancing = kwargs.get("loadbalancing", "nppc")
-    if loadbalancing not in valid_keys:
-        raise ValueError(
-            f"Error: loadbalancing type is not supported, supported types are {valid_keys}"
-        )
-    return loadbalancing
-
-
 # ------------------------------------------------------------------------------
 
 
--- pyphare/pyphare_tests/test_pharesee/test_geometry_3d.py
+++
@@ -1,1 +0,0 @@
-test_geometry_3d.py
