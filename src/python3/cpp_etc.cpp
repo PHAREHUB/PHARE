07b697a6b87e7522e533532b0b900322a0c73b35
PhilipDeegan <philip.deegan@gmail.com>
2024-07-30 18:58:16 +0200

    do not use h5py on restarts

--- src/python3/cpp_etc.cpp
+++ src/python3/cpp_etc.cpp
@@ -33,6 +33,9 @@ auto samrai_version()
 
 PYBIND11_MODULE(cpp_etc, m)
 {
+    auto samrai_restart_file = [](std::string path) {
+        return PHARE::amr::HierarchyRestarter::getRestartFileFullPath(path);
+    };
     py::class_<core::Span<double>, std::shared_ptr<core::Span<double>>>(m, "Span");
     py::class_<PyArrayWrapper<double>, std::shared_ptr<PyArrayWrapper<double>>, core::Span<double>>(
         m, "PyWrapper");
@@ -46,14 +49,33 @@ PYBIND11_MODULE(cpp_etc, m)
         return versions;
     });
 
-    m.def("samrai_restart_file", [](std::string path) {
-        return PHARE::amr::HierarchyRestarter::getRestartFileFullPath(path);
-    });
+    m.def("samrai_restart_file", samrai_restart_file);
 
     m.def("restart_path_for_time", [](std::string path, double timestamp) {
         return PHARE::amr::Hierarchy::restartFilePathForTime(path, timestamp);
     });
 
     m.def("phare_build_config", []() { return PHARE::build_config(); });
+
+    m.def("patch_data_ids", [&](std::string const& path) -> std::vector<int> {
+        _PHARE_WITH_HIGHFIVE({
+            auto const& restart_file = samrai_restart_file(path);
+            PHARE::hdf5::h5::HighFiveFile h5File{restart_file, HighFive::File::ReadOnly,
+                                                 /*para=*/false};
+            return h5File.read_data_set<int>("/phare/patch/ids");
+        });
+
+        throw std::runtime_error("PHARE not built with highfive support");
+    });
+    m.def("serialized_simulation_string", [&](std::string const& path) -> std::string {
+        _PHARE_WITH_HIGHFIVE({
+            auto const& restart_file = samrai_restart_file(path);
+            PHARE::hdf5::h5::HighFiveFile h5File{restart_file, HighFive::File::ReadOnly,
+                                                 /*para=*/false};
+            return h5File.read_attribute("/phare", "serialized_simulation");
+        });
+
+        throw std::runtime_error("PHARE not built with highfive support");
+    });
 }
 } // namespace PHARE::pydata
