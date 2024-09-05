951578d64d71e983d795e56b3530419358add353
PhilipDeegan <philip.deegan@gmail.com>
2024-09-05 16:31:39 +0200

    simpler dep handling + versions (#887)

--- res/cmake/dep.cmake
+++ res/cmake/dep.cmake
@@ -1,4 +1,22 @@
 
+
+function(phare_git_get_or_update name dir url branch)
+  if (NOT EXISTS ${dir})
+    execute_process(
+      COMMAND ${Git} clone ${url} ${dir} -b ${branch} --recursive --depth 1 --shallow-submodules
+    )
+  else()
+    if(devMode)
+      message("downloading latest ${name} updates")
+      execute_process(COMMAND ${Git} pull origin ${branch} WORKING_DIRECTORY ${dir})
+    endif(devMode)
+  endif()
+endfunction(phare_git_get_or_update)
+
+function(phare_github_get_or_update name dir repo branch)
+  phare_git_get_or_update(${name} ${dir} https://github.com/${repo} ${branch})
+endfunction(phare_github_get_or_update)
+
 include("${PHARE_PROJECT_DIR}/res/cmake/dep/cppdict.cmake")
 add_subdirectory(subprojects/cppdict)
 
