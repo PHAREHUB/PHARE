c4925857a981e573e384532b2184edfb9a59e610
UCaromel <caromelulysse@gmail.com>
2024-09-18 14:32:35 +0200

    gitignore and namespace update

--- src/amr/data/field/initializers/field_user_initializer.hpp
+++ src/amr/data/field/initializers/field_user_initializer.hpp
@@ -7,33 +7,30 @@
 #include <tuple>
 #include <memory>
 
-namespace PHARE 
+namespace PHARE::core
 {
-namespace core{    
-    class FieldUserFunctionInitializer {
-      public:
-            template<typename Field, typename GridLayout>
-            void static initialize(Field& field, GridLayout const& layout,
-                                      initializer::InitFunction<GridLayout::dimension> const& init)
-            {
-                auto const indices = layout.ghostStartToEndIndices(field, /*includeEnd=*/true);
-                auto const coords  = layout.template indexesToCoordVectors</*WithField=*/true>(
-                    indices, field, [](auto& gridLayout, auto& field_, auto const&... args) {
-                        return gridLayout.fieldNodeCoordinates(field_, gridLayout.origin(), args...);
-                    });
-            
-                std::shared_ptr<Span<double>> gridPtr // keep grid data alive
-                     = std::apply([&](auto&... args) { return init(args...); }, coords);
-                Span<double>& grid = *gridPtr;
-            
-                for (std::size_t cell_idx = 0; cell_idx < indices.size(); cell_idx++)
-                    std::apply([&](auto&... args) { field(args...) = grid[cell_idx]; },
-                               indices[cell_idx]);
-            }
-    };
-    
-} // namespace core
+class FieldUserFunctionInitializer
+{
+public:
+    template<typename Field, typename GridLayout>
+    void static initialize(Field& field, GridLayout const& layout,
+                           initializer::InitFunction<GridLayout::dimension> const& init)
+    {
+        auto const indices = layout.ghostStartToEndIndices(field, /*includeEnd=*/true);
+        auto const coords  = layout.template indexesToCoordVectors</*WithField=*/true>(
+            indices, field, [](auto& gridLayout, auto& field_, auto const&... args) {
+                return gridLayout.fieldNodeCoordinates(field_, gridLayout.origin(), args...);
+            });
 
-} // namespace PHARE
+        std::shared_ptr<Span<double>> gridPtr // keep grid data alive
+            = std::apply([&](auto&... args) { return init(args...); }, coords);
+        Span<double>& grid = *gridPtr;
+
+        for (std::size_t cell_idx = 0; cell_idx < indices.size(); cell_idx++)
+            std::apply([&](auto&... args) { field(args...) = grid[cell_idx]; }, indices[cell_idx]);
+    }
+};
+
+} // namespace PHARE::core
 
 #endif
