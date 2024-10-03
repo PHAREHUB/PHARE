4a087af77efdc1f177145ac03ce6a90831042c4e
PhilipDeegan <philip.deegan@gmail.com>
2024-10-03 20:08:07 +0200

    simple client calls for usable/settable

--- src/core/def.hpp
+++ src/core/def.hpp
@@ -1,13 +1,48 @@
 #ifndef PHARE_CORE_DEF_HPP
 #define PHARE_CORE_DEF_HPP
 
+#include <type_traits>
+
 #define NO_DISCARD [[nodiscard]]
 
+#if !defined(NDEBUG) || defined(PHARE_FORCE_DEBUG_DO)
+#define PHARE_DEBUG_DO(...) __VA_ARGS__
+#else
+#define PHARE_DEBUG_DO(...)
+#endif
 
 #define _PHARE_TO_STR(x) #x // convert macro text to string
 #define PHARE_TO_STR(x) _PHARE_TO_STR(x)
 
 #define PHARE_TOKEN_PASTE(x, y) x##y
 #define PHARE_STR_CAT(x, y) PHARE_TOKEN_PASTE(x, y)
+
+
+namespace PHARE::core
+{
+template<typename... Args>
+NO_DISCARD bool isUsable(Args const&... args)
+{
+    auto check = [](auto const& arg) {
+        if constexpr (std::is_pointer_v<std::decay_t<decltype(arg)>>)
+            return arg != nullptr;
+        else
+            return arg.isUsable();
+    };
+    return (check(args) && ...);
+}
+
+template<typename... Args>
+NO_DISCARD bool isSettable(Args const&... args)
+{
+    auto check = [](auto const& arg) {
+        if constexpr (std::is_pointer_v<std::decay_t<decltype(arg)>>)
+            return arg == nullptr;
+        else
+            return arg.isSettable();
+    };
+    return (check(args) && ...);
+}
+} // namespace PHARE::core
 
 #endif // PHARE_CORE_DEF_HPP
