8c4fa423b96a5a5b41d68351e252a0b2dac77d5f
deegan <philip.deegan@gmail.com>
2024-10-16 11:47:27 +0200

    refinement reserving capacity test

--- tests/amr/data/particles/refine/test_split.cpp
+++ tests/amr/data/particles/refine/test_split.cpp
@@ -5,6 +5,8 @@
 #include "core/utilities/types.hpp"
 #include "amr/data/particles/refine/split.hpp"
 
+#include "test_particledata_refine_basic_hierarchy.hpp"
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
@@ -12,7 +14,7 @@
 namespace
 {
 template<std::size_t dimension, std::size_t interpOrder, std::size_t refineParticlesNbr>
-using Splitter
+using Splitter_t
     = PHARE::amr::Splitter<PHARE::core::DimConst<dimension>, PHARE::core::InterpConst<interpOrder>,
                            PHARE::core::RefinedParticlesConst<refineParticlesNbr>>;
 
@@ -22,7 +24,7 @@ struct SplitterTest : public ::testing::Test
     SplitterTest() { Splitter splitter; }
 };
 
-using Splitters = testing::Types<Splitter<1, 1, 2>, Splitter<2, 1, 8> /*, Splitter<3, 1, 27>*/>;
+using Splitters = testing::Types<Splitter_t<1, 1, 2>, Splitter_t<2, 1, 8> /*, Splitter<3, 1, 27>*/>;
 
 TYPED_TEST_SUITE(SplitterTest, Splitters);
 
@@ -31,4 +33,20 @@ TYPED_TEST(SplitterTest, constexpr_init)
     constexpr TypeParam param{};
 }
 
+
+
 } // namespace
+
+
+using BasicHierarchy_t = BasicHierarchy<2, 3, ParticlesDataSplitType::interior, 4>;
+struct Hierarchy_t : public ::testing::Test, public BasicHierarchy_t
+{
+};
+
+TEST_F(Hierarchy_t, checkCapacityPostRefinement)
+{ // without reserve the capacity is 4096 and will likely have multiple allocations
+    auto domainParticles = domainParticlesForLevel(1);
+    ASSERT_EQ(domainParticles.size(), 1);
+    EXPECT_EQ(domainParticles[0]->size(), 2800);
+    EXPECT_EQ(domainParticles[0]->capacity(), 4032);
+}
