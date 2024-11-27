8a6987679b226dff19e8862d1e9752a1912110b2
deegan <philip.deegan@gmail.com>
2024-11-27 11:52:02 +0100

    release procedure
    new highfive github url

---
+++ res/cmake/release.cmake
@@ -0,0 +1,16 @@
+##
+# Release info
+#  KEEP SIMPLE!
+#
+
+# https://github.com/LLNL/SAMRAI/releases/tag/v-4-3-0
+SET(PHARE_SAMRAI_VERSION "v-4-3-0")
+
+# https://github.com/pybind/pybind11/releases/tag/v2.13.6
+SET(PHARE_PYBIND11_VERSION "v2.13.6")
+
+# https://github.com/BlueBrain/HighFive/releases/tag/v2.10.0
+SET(PHARE_HIGHFIVE_VERSION "master") # v2.10.0 is too old
+
+# https://github.com/LaboratoryOfPlasmaPhysics/cppdict/releases/tag/v1.0.0
+SET(PHARE_CPPDICT_VERSION "v1.0.0")
