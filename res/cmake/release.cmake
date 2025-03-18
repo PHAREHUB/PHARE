88ea734e3099f0a58fbe9d04febd45ec425ec6dd
deegan <philip.deegan@gmail.com>
2025-03-18 13:52:35 +0100

    cpp_etc needs highfive if available

--- res/cmake/release.cmake
+++ res/cmake/release.cmake
@@ -9,8 +9,8 @@ SET(PHARE_SAMRAI_VERSION "v-4-3-0")
 # https://github.com/pybind/pybind11/releases/tag/v2.13.6
 SET(PHARE_PYBIND11_VERSION "v2.13.6")
 
-# https://github.com/BlueBrain/HighFive/releases/tag/v2.10.0
-SET(PHARE_HIGHFIVE_VERSION "master") # v2.10.0 is too old
+# https://github.com/highfive-devs/highfive/releases/tag/v3.0.0-beta2
+SET(PHARE_HIGHFIVE_VERSION "v3.0.0-beta2")
 
 # https://github.com/LaboratoryOfPlasmaPhysics/cppdict/releases/tag/v1.0.0
 SET(PHARE_CPPDICT_VERSION "v1.0.0")
