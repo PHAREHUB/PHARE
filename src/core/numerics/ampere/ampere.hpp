8196b1acb12fc08b433ea095373d7936f523070f
UCaromel <caromelulysse@gmail.com>
2025-09-23 18:21:32 +0200

    Jx in ampere is double

--- src/core/numerics/ampere/ampere.hpp
+++ src/core/numerics/ampere/ampere.hpp
@@ -45,7 +45,7 @@ private:
         auto const& [_, By, Bz] = B();
 
         if constexpr (dimension == 1)
-            Jx(ijk...) = 0;
+            Jx(ijk...) = 0.0;
 
         if constexpr (dimension == 2)
             Jx(ijk...) = layout_->template deriv<Direction::Y>(Bz, {ijk...});
