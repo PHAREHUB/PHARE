8508e94644757e05fca0286d666a0d9b4a446145
roch smets <roch.smets@gmail.com>
2026-02-02 16:08:00 +0100

    modify the Getter of run.py to manage merged=True

--- pyphare/pyphare/core/operators.py
+++ pyphare/pyphare/core/operators.py
@@ -20,12 +20,12 @@ def _compute_dot_product(patch_datas, **kwargs):
 
 
 def _compute_sqrt(patch_datas, **kwargs):
-    ref_name = next(iter(patch_datas.keys()))
+    # ref_name = next(iter(patch_datas.keys())) TODO this is always "value"
 
     dset = np.sqrt(patch_datas["value"][:])
 
     return (
-        {"name": "value", "data": dset, "centering": patch_datas[ref_name].centerings},
+        {"name": "value", "data": dset, "centering": patch_datas["value"].centerings},
     )
 
 
