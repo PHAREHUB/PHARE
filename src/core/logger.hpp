ef004e033ff51c7d8fdcffa04429be2de846150f
deegan <philip.deegan@gmail.com>
2026-02-19 11:46:34 +0100

    timestamp test only in serial / fix caliper build

--- src/core/logger.hpp
+++ src/core/logger.hpp
@@ -1,7 +1,11 @@
 #ifndef PHARE_CORE_LOGGER_HPP
 #define PHARE_CORE_LOGGER_HPP
 
+#include "core/def.hpp" // IWYU pragma: keep
+
+#include <string>
 #include <cstdint>
+#include <utility>
 
 #if !defined(PHARE_LOG_LEVEL)
 #define PHARE_LOG_LEVEL 0 // 0 == off
@@ -13,6 +17,8 @@ constexpr static std::uint8_t LOG_LEVEL = PHARE_LOG_LEVEL;
 }
 
 #if !defined(NDEBUG) || defined(PHARE_FORCE_DEBUG_DO) || defined(PHARE_FORCE_LOG_LINE)
+#include <sstream>  // IWYU pragma: keep
+#include <iostream> // IWYU pragma: keep
 #define PHARE_LOG_LINE_STR(str)                                                                    \
     std::cout << __FILE__ << ":" << __LINE__ << " - " << str << std::endl;
 #define PHARE_LOG_LINE_SS(s) PHARE_LOG_LINE_STR((std::stringstream{} << s).str());
@@ -23,21 +29,18 @@ constexpr static std::uint8_t LOG_LEVEL = PHARE_LOG_LEVEL;
 #define PHARE_LOG_LINE PHARE_LOG_LINE_STR("")
 
 #if PHARE_WITH_CALIPER
-#include "caliper/cali.h"
+#include "caliper/cali.h" // IWYU pragma: keep
 
 #define PHARE_LOG_START(lvl, str) CALI_MARK_BEGIN(str)
 #define PHARE_LOG_STOP(lvl, str) CALI_MARK_END(str)
-#define PHARE_LOG_SCOPE(lvl, str) PHARE::scope_log __phare_scope##__line__(lvl, str)
+#define PHARE_LOG_SCOPE(lvl, str) PHARE::scope_log PHARE_STR_CAT(__phare_scope, __LINE__)(lvl, str)
 
 #else // !PHARE_WITH_CALIPER
 
-#include "core/utilities/logger/logger_defaults.hpp"
+#include "core/utilities/logger/logger_defaults.hpp" // IWYU pragma: keep
 
 
 #endif // PHARE_WITH_CALIPER
-
-#include <string>
-#include <utility>
 
 namespace PHARE
 {
