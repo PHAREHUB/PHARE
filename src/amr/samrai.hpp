dbf1d355ec25111b247b602b38983d25d0b6a431
deegan <philip.deegan@gmail.com>
2026-02-26 11:05:11 +0100

    allow arbitrary vector allocators

--- src/amr/samrai.hpp
+++ src/amr/samrai.hpp
@@ -1,17 +1,13 @@
-
 #ifndef PHARE_AMR_SAMRAI_HPP
 #define PHARE_AMR_SAMRAI_HPP
 
-#include "core/def/phlop.hpp" // scope timing
+#include "core/def/phare_mpi.hpp" // IWYU pragma: keep
+#include "core/utilities/types.hpp"
 
-#include "amr/wrappers/integrator.hpp"
-#include "core/utilities/mpi_utils.hpp"
-
-#include <SAMRAI/hier/VariableDatabase.h>
 #include <SAMRAI/tbox/RestartManager.h>
+#include <SAMRAI/hier/VariableDatabase.h>
 #include <SAMRAI/hier/PatchDataRestartManager.h>
 
-#include <memory>
 #include <iostream>
 
 namespace PHARE
@@ -49,5 +45,44 @@ public:
 
 } // namespace PHARE
 
+namespace PHARE::amr
+{
+
+
+template<typename T, typename A>
+auto& getVectorFromRestart(auto& db, auto const& path, std::vector<T, A>& vec)
+{
+    if (vec.size() == 0)
+        throw std::runtime_error("SAMRAI Restarts: vectors must be presized as expected");
+
+    if constexpr (std::is_same_v<T, double>)
+        db.getDoubleArray(path, vec.data(), vec.size());
+
+    else if constexpr (std::is_same_v<T, int>)
+        db.getIntegerArray(path, vec.data(), vec.size());
+
+    else
+        static_assert(core::dependent_false_v<T>,
+                      "SAMRAI getFromRestart Vector not supported, add it!");
+
+    return vec;
+};
+
+template<typename T, typename A>
+void putVectorToRestart(auto& db, auto const& path, std::vector<T, A> const& vec)
+{
+    if constexpr (std::is_same_v<T, double>)
+        db.putDoubleArray(path, vec.data(), vec.size());
+
+    else if constexpr (std::is_same_v<T, int>)
+        db.putIntegerArray(path, vec.data(), vec.size());
+
+    else
+        static_assert(core::dependent_false_v<T>,
+                      "SAMRAI putToRestart Vector not supported, add it!");
+};
+
+
+} // namespace PHARE::amr
 
 #endif /*PHARE_AMR_SAMRAI_HPP*/
