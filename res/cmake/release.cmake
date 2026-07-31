e2f9db5bfdeca67756bd83eef742c93a51041ec5
PhilipDeegan <philip.deegan@gmail.com>
2026-07-31 11:48:17 +0200

    Update pybind11 version to v3.0.4

--- res/cmake/release.cmake
+++ res/cmake/release.cmake
@@ -6,8 +6,8 @@
 # https://github.com/LLNL/SAMRAI/releases/tag/v-4-5-1
 SET(PHARE_SAMRAI_VERSION "v-4-5-1")
 
-# https://github.com/pybind/pybind11/releases/tag/v2.13.6
-SET(PHARE_PYBIND11_VERSION "v2.13.6")
+# https://github.com/pybind/pybind11/releases/tag/v3.0.4
+SET(PHARE_PYBIND11_VERSION "v3.0.4")
 
 # https://github.com/highfive-devs/highfive/releases/tag/v3.3.0
 SET(PHARE_HIGHFIVE_VERSION "v3.3.0")
