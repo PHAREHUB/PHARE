386722da1f074f61bf7fb8f862f9b442c73a873d
PhilipDeegan <philip.deegan@gmail.com>
2026-07-31 12:11:01 +0200

    Update cppdict version to v1.0.1

--- res/cmake/release.cmake
+++ res/cmake/release.cmake
@@ -12,5 +12,5 @@ SET(PHARE_PYBIND11_VERSION "v3.0.4")
 # https://github.com/highfive-devs/highfive/releases/tag/v3.3.0
 SET(PHARE_HIGHFIVE_VERSION "v3.3.0")
 
-# https://github.com/LaboratoryOfPlasmaPhysics/cppdict/releases/tag/v1.0.0
-SET(PHARE_CPPDICT_VERSION "v1.0.0")
+# https://github.com/LaboratoryOfPlasmaPhysics/cppdict/releases/tag/v1.0.1
+SET(PHARE_CPPDICT_VERSION "v1.0.1")
